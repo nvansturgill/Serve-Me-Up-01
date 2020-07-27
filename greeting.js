17f81e289508d8111f780bae79d5ce0142c81985
nvansturgill <vanburen.nia@gmail.com>
2020-07-26 20:17:38 -0400

    Add `greeting` variable and link with `module.exports` to `userInput.js`

---
+++ greeting.js
@@ -0,0 +1,5 @@
+// Init greeting variable with string value
+let greeting = "Hello, user. Let's play a game.";
+
+// Set-up `greeting` varibale for export to `userInput.js`
+module.exports = greeting;
--- userInput.js
+++ userInput.js
@@ -3,3 +3,9 @@ const fs = require("fs");
 
 // Require Node.js `inquirer` npm dependency
 const inquirer = require("inquirer");
+
+// Require `greeting.js` file
+const greeting = require("./greeting.js");
+
+// Console log `greeting` variable
+console.log(greeting);
