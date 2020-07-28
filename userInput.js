d6b187d2e9b0bc331c1bbac82245e9bb8ad9bb2a
nvansturgill <vanburen.nia@gmail.com>
2020-07-27 21:56:30 -0400

    Create server to accept user input

--- userInput.js
+++ userInput.js
@@ -6,6 +6,21 @@ const http = require("http");
 const inquirer = require("inquirer");
 const greeting = require("./greeting.js");
 
+// Declare host and port variables for server
+const hostname = "127.0.0.1";
+const port = "3000";
+
+// Create server
+const server = http.createServer(function (req, res) {
+  // Declare statusCode 200: server without errors
+  res.statusCode = 200;
+  // res.setHeader("Content-type", "text/html");
+});
+
+// Declare variable to confirm server start
+const play = "Look! a wild server appeared on " + port;
+console.log(play);
+
 // Console log `greeting` variable
 console.log(greeting);
 
@@ -20,5 +35,5 @@ function userQuestions() {
   ]);
 }
 
-// Call and print `userQuestion` to console
-userQuestions();
+// Call and `userQuestion` to console
+// userQuestions();
